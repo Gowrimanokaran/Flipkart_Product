da43b7d0184772eec9310e443a70fdfca19999e0
Gowrimanokaran <manovj9480@gmail.com>
2025-10-30 22:42:10 +0530

    New git repo is created and file is added

---
+++ src/test/java/BrowserSetup/Browser.java
@@ -0,0 +1,13 @@
+package BrowserSetup;
+
+public class Browser {
+
+
+
+    public static void main(String[] args)
+    {
+
+
+        System.out.println(" Hey Welcome to Git and Github Course ");
+    }
+}
